7b22a153839931d7b42a66ce230aa5ac5ca16289
Abdelrahman Elbehery <abdo.m7med.89@gmail.com>
2017-05-13 15:43:56 -0700

    Fixed an error in the graph class

--- Maze Solver/src/graph.h
+++ Maze Solver/src/graph.h
@@ -23,16 +23,16 @@ template<typename userClass>
 class Graph
 {
 	//TODO: define the DESTRUCTOR, Test memory leaks
-	std::map<userClass, GraphNode<unsigned long>*> nodes;
+	std::map<userClass, GraphNode<userClass>*> nodes;
 													//A graph keeps track of all of its nodes via a map ds, each key returns a pointer to a graph object in the mem
 	unsigned long nodesCount;
 													//Holds the number of nodes inside the graph
 public:
 	Graph() { nodesCount = 0; }
 													//Defult constructor
-	void addNode(unsigned long newNodeID);
+	void addNode(userClass newNodeID);
 													//User can add node given only its unique ID
-	void addEdge(unsigned long leftNodeID, unsigned long rightNodeID,unsigned long weight, bool directed = false);
+	void addEdge(userClass leftNodeID, userClass rightNodeID,userClass weight, bool directed = false);
 													//User can connect two nodes given both of their unique ID, lhs->rhs, Throws exception if one of the nodes does not exisit
 													//directed is set to true in case this edge is a directed edge
 
@@ -44,14 +44,14 @@ public:
 //-----------------------------------
 
 template<typename userClass>
-void Graph<userClass>::addNode(unsigned long  newNodeID)
+void Graph<userClass>::addNode(userClass  newNodeID)
 {
 	if (nodes[newNodeID] != 0)
 		throw;
 		//NODE ALREADY EXISIT
 
 	//DEFINE DESTRUCTOR, WATCH MEMORY LEAKS
-	nodes[newNodeID] = new GraphNode<unsigned long>(newNodeID);
+	nodes[newNodeID] = new GraphNode<userClass>(newNodeID);
 	nodesCount++;
 }
 
@@ -60,10 +60,10 @@ If the edge is a directed edge then only LeftNode --w--> RightNode
 Otherwise both nodes are connected to each other
 */
 template<typename userClass>
-void Graph<userClass>::addEdge(unsigned long  leftNodeID, unsigned long rightNodeID, unsigned long weight, bool directed)
+void Graph<userClass>::addEdge(userClass  leftNodeID, userClass rightNodeID, userClass weight, bool directed)
 {
-	GraphNode<unsigned long> *lhsPtr = nodes[leftNodeID];
-	GraphNode<unsigned long> *rhsPtr = nodes[rightNodeID];
+	GraphNode<userClass> *lhsPtr = nodes[leftNodeID];
+	GraphNode<userClass> *rhsPtr = nodes[rightNodeID];
 	if (lhsPtr == 0 || rhsPtr == 0)
 		throw;
 		//ONE OF THE NODES DOES NOT EXISIT, THE METHOD DOES NOT AUTOMATICALLY CREATE NODE IN CASE THEY DONT EXISIT
